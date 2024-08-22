9d6b6625953b4ade213c883f44903e1a44b320b2
ENTER-ALT <enter.alt.591@gmail.com>
2024-08-22 23:18:07 +0000

    update executor logging

--- src/executor.py
+++ src/executor.py
@@ -1,4 +1,7 @@
 import subprocess
+from operator import rshift
+
+from src.logger import Logger, LoggerStatus
 
 
 class Executor:
@@ -6,5 +9,6 @@ class Executor:
     def execute_command(command: list):
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         if result.stderr:
+            Logger.add_record(text=result.stderr, status=LoggerStatus.ERROR)
             raise ValueError(result.stderr + "\n" + ' '.join(command))
         return result.stdout
