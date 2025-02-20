a228a40b5284de3f1239f9dc10aab50e1196dcb6
ENTER-ALT <enter.alt.591@gmail.com>
2025-02-20 14:13:54 +0100

    qemu/ optimized vm manager, made it instantiable

--- src/vm_manager.py
+++ src/vm_manager.py
@@ -1,108 +1,59 @@
 from data_types.configs.vm_config import VMConfig
-from data_types.configs.storage_controller_config import StorageControllerConfig
-from data_types.configs.medium_attachment_config import MediumAttachmentConfig, DriveType
-from data_types.configs.medium_config import MediumConfig, HDDMediumConfig
 from shell_executor import ShellExecutor
 from string_extractor import StringExtractor
+from file_validator import FileValidator
 from command_shortcuts import *
 
 
 class VMManager:
     
-    @staticmethod
-    def vm_exists(vm_name: str):
-        command_message = [
-            VBOXMANAGE_COMMAND,
-            LIST_COMMAND,
-            VM_OPTION
-        ]
+    def __init__(self, vm_config: VMConfig):
+        self.vm_config: VMConfig = vm_config
+        self.linked_images = []
 
-        output = ShellExecutor.execute_command(command_message)
-        return StringExtractor.vm_exists(vm_name, output)
+    def vm_exists(self):
+        return FileValidator.validate_file_exists(self.vm_config.image_file_name)
     
-    @staticmethod  
-    def delete_vm(vm_name: str):
+    def delete_vm(self):
         command_message = [
             VBOXMANAGE_COMMAND,
             UNREGISTERVM_COMMAND,
-            vm_name,
+            self.vm_config.name,
             DELETE_OPTION
         ]
 
         ShellExecutor.execute_command(command_message)
     
-    @staticmethod
-    def create_vm(vm_config: VMConfig):
+    def create_vm(self):
         command_message = [
             VBOXMANAGE_COMMAND,
             CREATEVM_COMMAND,
-            NAME_OPTION, vm_config.name,
-            OSTYPE_OPTION, vm_config.os_type,
+            NAME_OPTION, self.vm_config.name,
+            OSTYPE_OPTION, self.vm_config.os_type,
             REGISTER_OPTION
         ]
 
         output = ShellExecutor.execute_command(command_message)
-        vm_config.folder = StringExtractor.extract_folder_path_from_create_vm_output(output)
+        self.vm_config.folder = StringExtractor.extract_folder_path_from_create_vm_output(output)
 
-    @staticmethod
-    def init_vm(vm_config: VMConfig):
+    def init_vm(self):
         command_message = [
-            VBOXMANAGE_COMMAND,
-            MODIFYVM_COMMAND,
-            vm_config.name,
-            DESCRIPTION_OPTION, vm_config.description,
-            MEMORY_OPTION, vm_config.ram,
-            VRAM_OPTION, vm_config.vram,
-            CPUS_OPTION, vm_config.cpus,
-            NIC1_OPTION, vm_config.nic1,
-            AUTOSTART_ENABLED_OPTION, vm_config.autostart_enabled,
-            DRAG_AND_DROP_OPTION, vm_config.drag_and_drop,
-            BRIDGE_ADAPTER1_OPTION, vm_config.bridge_adapter
         ]
 
         ShellExecutor.execute_command(command_message)
 
-    @staticmethod
-    def create_storage_controller(vm_config: VMConfig, storage_controller_config: StorageControllerConfig):
-        command_message = [
-            VBOXMANAGE_COMMAND,
-            STORAGECTL_COMMAND,
-            vm_config.name,
-            NAME_OPTION, storage_controller_config.name,
-            ADD_OPTION, storage_controller_config.add,
-            CONTROLLER_OPTION, storage_controller_config.controller
-        ]
+    def create_image(self):
+        # Implementation for creating an image
+        pass
 
-        ShellExecutor.execute_command(command_message)
+    def install_OS(self):
+        # Implementation for installing the OS
+        pass
 
-    @staticmethod
-    def create_hdd_medium(medium_config: HDDMediumConfig):
-        command_message = [
-            VBOXMANAGE_COMMAND,
-            CREATEMEDIUM_COMMAND,
-            medium_config.medium_type,
-            FILENAME_OPTION, medium_config.filename,
-            SIZE_OPTION, medium_config.size,
-            FORMAT_OPTION, medium_config.format,
-            VARIANT_OPTION, medium_config.variant
-        ]
+    def create_linked_images(self, quantity):
+        # Implementation for creating linked images
+        pass
 
-        ShellExecutor.execute_command(command_message)
-
-    @staticmethod
-    def attach_medium_to_controller(
-            vm_config: VMConfig,
-            storage_controller_config: StorageControllerConfig,
-            medium_config: MediumConfig,
-            medium_attachment_config: MediumAttachmentConfig):
-        command_message = [
-            VBOXMANAGE_COMMAND,
-            STORAGEATTACH_COMMAND,
-            vm_config.name,
-            STORAGECTL_OPTION, storage_controller_config.name,
-            PORT_OPTION, medium_attachment_config.port,
-            TYPE_OPTION, medium_attachment_config.drive_type,
-            MEDIUM_OPTION, medium_config.filename
-        ]
-
-        ShellExecutor.execute_command(command_message)
+    def run_VM(self):
+        # Implementation for running the VM
+        pass
