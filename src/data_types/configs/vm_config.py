e8f27443b8ddda44880df2a2ab324a16abae4574
ENTER-ALT <enter.alt.591@gmail.com>
2025-02-20 12:56:27 +0100

    qemu/ redo the config objects

--- src/data_types/configs/vm_config.py
+++ src/data_types/configs/vm_config.py
@@ -1,65 +1,25 @@
-from enum import Enum
-
-class NICMode(Enum):
-    BRIDGED = "bridged"
-    NAT = "nat"
-
-class DragAndDropMode(Enum):
-    DISABLED = "disabled"
-    BIDIRECTIONAL = "bidirectional"
-
-class OSType(Enum):
-    LINUX_64 = "Linux_64"
-
 class VMConfig:
     def __init__(
         self,
-        name="o10",
-        folder="",
-        os_type=OSType.LINUX_64.value,
-        description="allo",
-        hdd_size="10240",
-        ram="1024",
-        vram="8",
-        cpus="1",
-        nic1=NICMode.BRIDGED.value,
-        autostart_enabled="off",
-        drag_and_drop=DragAndDropMode.BIDIRECTIONAL.value,
-        bridge_adapter="wlan0",
-        iso_file="",
-        disk_name="disk.vmdk"
+        name="example_name",
+        image_size_gb="15",
+        ram_gb="2",
+        image_file_name="example.img",
+        iso_file_name=""
     ) -> None:
         self.name = name
-        self.folder = folder
-        self.os_type = os_type
-        self.description = description
-        self.hdd_size = hdd_size
-        self.ram = ram
-        self.vram = vram
-        self.cpus = cpus
-        self.nic1 = nic1
-        self.autostart_enabled = autostart_enabled
-        self.drag_and_drop = drag_and_drop
-        self.bridge_adapter = bridge_adapter
-        self.iso_file = iso_file
-        self.disk_name = disk_name
+        self.image_size_gb = image_size_gb
+        self.ram_gb = ram_gb
+        self.image_file_name = image_file_name
+        self.iso_file_name = iso_file_name
 
     def copy(self, idx):
         return VMConfig(
             name=self.name + f"_{idx}",
-            folder=self.folder,
-            os_type=self.os_type,
-            description=self.description,
-            hdd_size=self.hdd_size,
-            ram=self.ram,
-            vram=self.vram,
-            cpus=self.cpus,
-            nic1=self.nic1,
-            autostart_enabled=self.autostart_enabled,
-            drag_and_drop=self.drag_and_drop,
-            bridge_adapter=self.bridge_adapter,
-            iso_file=self.iso_file,
-            disk_name=self.disk_name
+            image_size_gb=self.image_size_gb,
+            ram_gb=self.ram_gb,
+            image_file_name=self.image_file_name,
+            iso_file_name=self.iso_file_name
         )
 
     @classmethod
